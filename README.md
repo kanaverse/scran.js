bb5eaa494ae908f9ce813eeff17fa5b8d2564ff8
jkanche <jayaram.kancherla@gmail.com>
2021-06-21 22:48:28 -0700

    init: first commit, basic structure generating data in wasm space

---
+++ README.md
@@ -0,0 +1,9 @@
+```
+git submodule init
+git submodule update --recursive
+
+inside libscran
+
+cmake .
+make
+```
---
+++ src/scran.js
@@ -0,0 +1,69 @@
+class scran {
+    constructor (data, options, wasm) {
+        // wasm module initialized in the browser
+        this.wasm = wasm;
+        
+        // this.data = data;
+        // for now generate data
+        this.data = this.generateData(10 * 10);
+
+        // holds any options
+        this.options = options;
+
+        // keep track of data objects
+        this._internalMemTracker = {};
+
+        this._heapMap = {
+            "Float64Array": {
+                "wasm": "HEAPF64"
+            }, 
+            "Float32Array": {
+                "wasm": "HEAP32"
+            }
+        }
+    }
+
+    getRandomArbitrary() {
+        return Math.random();
+    }
+
+    generateData(size) {
+        const arr = this.createMemorySpace(size, Float64Array);
+        arr.set(arr.map(() => this.getRandomArbitrary()));
+        return arr;
+    }
+
+    // from epiviz
+    _generateGuid() {
+        var chars =
+          "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var result = "";
+        var size = 5;
+
+        for (var i = 0; i < size; ++i) {
+          result += chars[Math.round(Math.random() * (chars.length - 1))];
+        }
+        return "var-" + result;
+      }
+
+    createMemorySpace(size, type, key) {
+
+        if (!key) {
+            key = this._generateGuid();
+        }
+
+        if (type == "Float64Array") {
+            let ptr = wasm._malloc(size * 8);
+
+            const arr = new Float64Array(
+                this.wasm[this._heapMap[type]["wasm"]].buffer,
+                ptr,
+                size
+            );
+
+            this._internalMemTracker[key] = [ptr, size];
+
+            return arr;
+        }
+    }
+}
