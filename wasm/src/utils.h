c5037d6c6de3dd0bce880c78eeef1002f3e8d845
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-07-01 23:49:12 -0700

    Added doxygen docs for the pointer casting utility.

--- wasm/src/utils.h
+++ wasm/src/utils.h
@@ -4,6 +4,17 @@
 #include <vector>
 #include <cstdint>
 
+/**
+ * Create a vector of pointers based on the WASM heap offsets.
+ * 
+ * @tparam T Type of the pointers to cast the offsets to.
+ *
+ * @param ptr Offset to the start of a unsigned 32-bit integer array.
+ * Each integer is itself assumed to be an offset to another array of type `T`.
+ * @param n Number of integers in the array referenced by `ptr`.
+ *
+ * @return A `std::vector<T>` of length `n`, containing pointers to various arrays.
+ */
 template<typename T>
 std::vector<T> cast_vector_of_pointers(uintptr_t ptr, size_t n) {
     std::vector<T> store(n);
