44582936b3415518a7c752acf4a933a3f8b3301c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-07-09 00:20:36 -0700

    Added code to actuall perform SNN clustering.

---
+++ wasm/src/cluster_snn_graph.cpp
@@ -0,0 +1,28 @@
+#include <emscripten/bind.h>
+
+#include "scran/clustering/ClusterSNNGraph.hpp"
+#include <algorithm>
+
+void cluster_snn_graph(int ndim, int ncells, uintptr_t mat, int k, double resolution, uintptr_t membership) {
+    scran::ClusterSNNGraph clust;
+    clust.builder.set_neighbors(k);
+
+    const double* ptr = reinterpret_cast<const double*>(mat);
+    auto output = clust.run_multilevel(ndim, ncells, ptr, resolution);
+
+    const auto& chosen = output.membership[output.max];
+    std::copy(chosen.begin(), chosen.end(), reinterpret_cast<int32_t*>(membership));
+
+    return;
+}
+
+/**
+ * @cond
+ */
+EMSCRIPTEN_BINDINGS(cluster_snn_graph) {
+    emscripten::function("cluster_snn_graph", &cluster_snn_graph);
+}
+/**
+ * @endcond
+ */
+
