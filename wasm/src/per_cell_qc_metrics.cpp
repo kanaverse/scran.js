7f5be05bec31053930a1591ad089163b854e6c84
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-07-14 12:51:19 -0700

    Begin trial attempts to parallelize.

--- wasm/src/per_cell_qc_metrics.cpp
+++ wasm/src/per_cell_qc_metrics.cpp
@@ -4,9 +4,12 @@
 #include "utils.h"
 
 #include "scran/quality_control/PerCellQCMetrics.hpp"
+#include "tatami/base/DelayedSubsetBlock.hpp"
 
 #include <vector>
 #include <cstdint>
+#include <thread>
+#include <cmath>
 
 /**
  * Compute some basic -cell QC metrics.
@@ -28,16 +31,47 @@ void per_cell_qc_metrics(const NumericMatrix& mat,
                          uintptr_t subsets, 
                          uintptr_t sums, 
                          uintptr_t detected, 
-                         uintptr_t proportions) 
+                         uintptr_t proportions,
+                         int nworkers) 
 {
-    scran::PerCellQCMetrics qc;
+    auto insub = cast_vector_of_pointers<const uint8_t*>(subsets, nsubsets);
+    auto outsum = reinterpret_cast<double*>(sums);
+    auto outdet = reinterpret_cast<int32_t*>(detected);
+    auto outprop = cast_vector_of_pointers<double*>(proportions, nsubsets);
 
-    qc.run(mat.ptr.get(), 
-           cast_vector_of_pointers<const uint8_t*>(subsets, nsubsets),
-           reinterpret_cast<double*>(sums),  
-           reinterpret_cast<int32_t*>(detected),  
-           cast_vector_of_pointers<double*>(proportions, nsubsets)
-    );
+#ifdef __EMSCRIPTEN_PTHREADS__ 
+    if (nworkers > 1) {
+        int jobs_per_worker = std::ceil(static_cast<double>(mat.ncol())/nworkers);
+        std::vector<std::thread> workers;
+        workers.reserve(nworkers);
+        int first = 0; 
+
+        auto executor = [&](int left, int right) -> void {
+            auto propcopy = outprop;
+            for (auto& p : propcopy) { 
+                p += left;
+            }
+            auto current = tatami::make_DelayedSubsetBlock<1>(mat.ptr, left, right);
+            scran::PerCellQCMetrics qc;
+            qc.run(mat.ptr.get(), insub, outsum + left, outdet + left, std::move(propcopy));
+        };
+
+        for (int w = 0; w < nworkers && first < mat.ncol(); ++w) {
+            int last = std::min(first + jobs_per_worker, static_cast<int>(mat.ncol()));
+            workers.emplace_back(std::thread(executor, first, last));
+        }
+
+        for (auto& wrk : workers) {
+            wrk.join();
+        }
+
+    } else {
+#endif
+        scran::PerCellQCMetrics qc;
+        qc.run(mat.ptr.get(), std::move(insub), outsum, outdet, std::move(outprop));
+#ifdef __EMSCRIPTEN_PTHREADS__ 
+    }
+#endif
 
     return;
 }
