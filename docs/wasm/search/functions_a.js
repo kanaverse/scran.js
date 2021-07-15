a054349e4dec71681ce3cb94a7195221b05fcb5e
LTLA <LTLA@users.noreply.github.com>
2021-07-15 01:27:01 +0000

    Deploying to gh-pages from @ jkanche/scran.js@e355e0ada956d6058feaede5fab48ab6f4911d5e 🚀

--- docs/wasm/search/functions_a.js
+++ docs/wasm/search/functions_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['row_57',['row',['../structNumericMatrix.html#a428afa5858a25c1c6b8861900e0dad7b',1,'NumericMatrix']]],
-  ['run_5fpca_58',['run_pca',['../run__pca_8cpp.html#ae1d199a55cbba67e4a58f46fdd9d23f3',1,'run_pca.cpp']]]
+  ['run_5fpca_58',['run_pca',['../run__pca_8cpp.html#a621610cf19d42bda18cb5f2ca9c42c54',1,'run_pca.cpp']]]
 ];
