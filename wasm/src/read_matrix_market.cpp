245072df6ef8aee58bb23f0343e846af5d35b41c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-07-16 16:55:13 -0700

    Add autodetection support for Gzipped files.

--- wasm/src/read_matrix_market.cpp
+++ wasm/src/read_matrix_market.cpp
@@ -5,10 +5,19 @@
 #include <string>
 
 NumericMatrix read_matrix_market(std::string input) {
-    auto stuff = tatami::MatrixMarket::load_layered_sparse_matrix(input.c_str());
-    NumericMatrix output(std::move(stuff.matrix));
-    output.permutation = stuff.permutation;
-    return output;
+    auto process = [&](auto& stuff) {
+        NumericMatrix output(std::move(stuff.matrix));
+        output.permutation = stuff.permutation;
+        return output;
+    };
+
+    if (input.length() >= 3 && (0 == input.compare(input.length() - input.length(), input.length(), ".gz"))) {
+        auto stuff = tatami::MatrixMarket::load_layered_sparse_matrix_gzip(input.c_str());
+        return process(stuff);
+    } else {
+        auto stuff = tatami::MatrixMarket::load_layered_sparse_matrix(input.c_str());
+        return process(stuff);
+    }
 }
 
 /**
