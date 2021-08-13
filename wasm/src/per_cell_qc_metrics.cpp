5d22dab8d1faf9b607ef960107c01e3a563d7a96
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-08-12 22:12:38 -0700

    Greatly streamlined data transfer for QC steps.

--- wasm/src/per_cell_qc_metrics.cpp
+++ wasm/src/per_cell_qc_metrics.cpp
@@ -2,6 +2,7 @@
 
 #include "NumericMatrix.h"
 #include "utils.h"
+#include "PerCellQCMetrics_Results.h"
 
 #include "scran/quality_control/PerCellQCMetrics.hpp"
 
@@ -9,37 +10,25 @@
 #include <cstdint>
 
 /**
- * Compute some basic -cell QC metrics.
+ * @file per_cell_qc_metrics.cpp
+ *
+ * @brief Compute per-cell QC metrics from the count matrix.
+ */
+
+/**
+ * Compute some basic per-cell QC metrics.
  *
  * @param mat A `NumericMatrix` object containing features in rows and cells in columns.
  * @param nsubsets Number of feature subsets to be considered.
  * @param[in] subsets Offset to a 2D array of `uint8_t`s with number of rows and columns equal to `mat.nrow()` and `nsubsets`, respectively.
  * The array should be column-major where each column corresponds to a feature subset and each value indicates whether each feature in `mat` belongs to that subset.
  *
- * @param[out] sums Offset to an output buffer of `double`s with `mat.ncol()` elements, containing the total sum of counts for all cells.
- * @param[out] detected Offset to an output buffer of `int32_t`s with `mat.ncol()` elements, containing the number of detected features for all cells.
- * @param[out] proportions Offset to a 2D array of `double`s with number of rows and columns equal to `mat.ncol()` and `subsets`, respectively.
- * The array should be column-major where each column corresponds to a subset and each value contains the proportion of counts assigned to that subset in each cell.
- *
- * @return All arrays in `sums`, `detected`, `proportions` and `discard_overall` are filled.
+ * @return A `PerCellQCMetrics_Results` object that can be interrogated to obtain each QC metric.
  */
-void per_cell_qc_metrics(const NumericMatrix& mat, 
-                         int nsubsets, 
-                         uintptr_t subsets, 
-                         uintptr_t sums, 
-                         uintptr_t detected, 
-                         uintptr_t proportions) 
-{
+PerCellQCMetrics_Results per_cell_qc_metrics(const NumericMatrix& mat, int nsubsets, uintptr_t subsets) {
     scran::PerCellQCMetrics qc;
-
-    qc.run(mat.ptr.get(), 
-           extract_column_pointers<const uint8_t*>(subsets, mat.nrow(), nsubsets),
-           reinterpret_cast<double*>(sums),  
-           reinterpret_cast<int32_t*>(detected),  
-           extract_column_pointers<double*>(proportions, mat.ncol(), nsubsets)
-    );
-
-    return;
+    auto store = qc.run(mat.ptr.get(), extract_column_pointers<const uint8_t*>(subsets, mat.nrow(), nsubsets));
+    return PerCellQCMetrics_Results(std::move(store));
 }
 
 /**
@@ -47,6 +36,12 @@ void per_cell_qc_metrics(const NumericMatrix& mat,
  */
 EMSCRIPTEN_BINDINGS(per_cell_qc_metrics) {
     emscripten::function("per_cell_qc_metrics", &per_cell_qc_metrics);
+
+    emscripten::class_<PerCellQCMetrics_Results>("PerCellQCMetrics_Results")
+        .function("sums", &PerCellQCMetrics_Results::sums)
+        .function("detected", &PerCellQCMetrics_Results::detected)
+        .function("subset_proportions", &PerCellQCMetrics_Results::subset_proportions)
+        ;
 }
 /**
  * @endcond 
