86f8fe5eeafaafbecb07a3921bdca2ba127cfc35
LTLA <LTLA@users.noreply.github.com>
2021-12-10 05:58:08 +0000

    Deploying to gh-pages from @ jkanche/scran.js@7e9977cabe02dc8cd266d69d78e26399f5cbfaf9 🚀

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,9 +1,8 @@
 var searchData=
 [
-  ['clone_0',['clone',['../structTsneStatus.html#a6f2dc0a5d02a6534fb91cd71b38f7724',1,'TsneStatus::clone()'],['../structUmapStatus.html#af204ea69474edfdcb55066f2bfc35300',1,'UmapStatus::clone()']]],
-  ['cluster_5fsnn_5fgraph_1',['cluster_snn_graph',['../cluster__snn__graph_8cpp.html#a6706a215f7cc8e2cfc3f4e30ede99e11',1,'cluster_snn_graph.cpp']]],
-  ['cluster_5fsnn_5fgraph_5ffrom_5fgraph_2',['cluster_snn_graph_from_graph',['../cluster__snn__graph_8cpp.html#a88a4b4f13f5bbb333357355fd454ae97',1,'cluster_snn_graph.cpp']]],
-  ['cluster_5fsnn_5fgraph_5ffrom_5findex_3',['cluster_snn_graph_from_index',['../cluster__snn__graph_8cpp.html#a2b003264803b459a68cdf33322ea46f2',1,'cluster_snn_graph.cpp']]],
-  ['cohen_4',['cohen',['../structScoreMarkers__Results.html#a77ce6eb68fd05cff4a5218e4a6fb549c',1,'ScoreMarkers_Results']]],
-  ['column_5',['column',['../structNumericMatrix.html#a6f0a43d05259f91bcbe5710ee8151922',1,'NumericMatrix']]]
+  ['cluster_5fsnn_5fgraph_0',['cluster_snn_graph',['../cluster__snn__graph_8cpp.html#a6706a215f7cc8e2cfc3f4e30ede99e11',1,'cluster_snn_graph.cpp']]],
+  ['cluster_5fsnn_5fgraph_5ffrom_5fgraph_1',['cluster_snn_graph_from_graph',['../cluster__snn__graph_8cpp.html#a88a4b4f13f5bbb333357355fd454ae97',1,'cluster_snn_graph.cpp']]],
+  ['cluster_5fsnn_5fgraph_5ffrom_5findex_2',['cluster_snn_graph_from_index',['../cluster__snn__graph_8cpp.html#a2b003264803b459a68cdf33322ea46f2',1,'cluster_snn_graph.cpp']]],
+  ['cohen_3',['cohen',['../structScoreMarkers__Results.html#a77ce6eb68fd05cff4a5218e4a6fb549c',1,'ScoreMarkers_Results']]],
+  ['column_4',['column',['../structNumericMatrix.html#a6f0a43d05259f91bcbe5710ee8151922',1,'NumericMatrix']]]
 ];
