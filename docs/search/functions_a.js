629732ec0b2dee86b2f4232caedec7b137a81427
LTLA <LTLA@users.noreply.github.com>
2021-12-13 00:35:59 +0000

    Deploying to gh-pages from @ jkanche/scran.js@3ed956dcc76618babed472d53a52b7d5e749b3e6 🚀

--- docs/search/functions_a.js
+++ docs/search/functions_a.js
@@ -3,7 +3,7 @@ var searchData=
   ['ncol_0',['ncol',['../structNumericMatrix.html#ad21bf001b87002dcb346d9fcdd4e8a60',1,'NumericMatrix']]],
   ['nrow_1',['nrow',['../structNumericMatrix.html#aca9949603c5969178a3d43eb1a352f63',1,'NumericMatrix']]],
   ['num_5fepochs_2',['num_epochs',['../structUmapStatus.html#a1ea0cca74dbc213ad1b402965b96417c',1,'UmapStatus']]],
-  ['num_5fobs_3',['num_obs',['../structTsneStatus.html#a445df79a036ee010bb34ba09a383cb60',1,'TsneStatus::num_obs()'],['../structUmapStatus.html#ac99bc66d67b16a8fe90a72319cd7243d',1,'UmapStatus::num_obs()']]],
+  ['num_5fobs_3',['num_obs',['../structNeighborIndex.html#abcff91730ec1285b7840d223fd4702c4',1,'NeighborIndex::num_obs()'],['../structTsneStatus.html#a445df79a036ee010bb34ba09a383cb60',1,'TsneStatus::num_obs()'],['../structUmapStatus.html#ac99bc66d67b16a8fe90a72319cd7243d',1,'UmapStatus::num_obs()']]],
   ['number_4',['number',['../structClusterSNNGraphMultiLevel__Result.html#a351e31c8c57ab4dc50cb63a63598f4c8',1,'ClusterSNNGraphMultiLevel_Result']]],
   ['numericmatrix_5',['NumericMatrix',['../structNumericMatrix.html#ad2a44d614c57a6bf2a3e400167ebb3e8',1,'NumericMatrix::NumericMatrix(const tatami::NumericMatrix *p)'],['../structNumericMatrix.html#a03837f6ccdc2da8a2caa7e2772a2a0fd',1,'NumericMatrix::NumericMatrix(std::shared_ptr&lt; const tatami::NumericMatrix &gt; p)'],['../structNumericMatrix.html#af316832630f2a6bd5bd815ffce78018c',1,'NumericMatrix::NumericMatrix(int nr, int nc, uintptr_t values)']]]
 ];
