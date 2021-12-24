116c1d4b0bf54a36f53adbde38a8995db19ad152
LTLA <LTLA@users.noreply.github.com>
2021-12-24 01:31:11 +0000

    Deploying to gh-pages from @ jkanche/scran.js@29d4487dd0b05a904ea83a1b5a37d6eb478944ad 🚀

--- docs/search/all_8.js
+++ docs/search/all_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['log_5fnorm_5fcounts_0',['log_norm_counts',['../log__norm__counts_8cpp.html#a67c799ea48a1c508b5a8455a996df103',1,'log_norm_counts.cpp']]],
-  ['log_5fnorm_5fcounts_2ecpp_1',['log_norm_counts.cpp',['../log__norm__counts_8cpp.html',1,'']]]
+  ['lfc_0',['lfc',['../structScoreMarkers__Results.html#a6b82560ee5ce8461e0b4ccf9649c1f47',1,'ScoreMarkers_Results']]],
+  ['log_5fnorm_5fcounts_1',['log_norm_counts',['../log__norm__counts_8cpp.html#a67c799ea48a1c508b5a8455a996df103',1,'log_norm_counts.cpp']]],
+  ['log_5fnorm_5fcounts_2ecpp_2',['log_norm_counts.cpp',['../log__norm__counts_8cpp.html',1,'']]]
 ];
--- docs/search/functions_8.js
+++ docs/search/functions_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['log_5fnorm_5fcounts_0',['log_norm_counts',['../log__norm__counts_8cpp.html#a67c799ea48a1c508b5a8455a996df103',1,'log_norm_counts.cpp']]]
+  ['lfc_0',['lfc',['../structScoreMarkers__Results.html#a6b82560ee5ce8461e0b4ccf9649c1f47',1,'ScoreMarkers_Results']]],
+  ['log_5fnorm_5fcounts_1',['log_norm_counts',['../log__norm__counts_8cpp.html#a67c799ea48a1c508b5a8455a996df103',1,'log_norm_counts.cpp']]]
 ];
