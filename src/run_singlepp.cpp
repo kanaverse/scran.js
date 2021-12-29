ae09e2cca5c90317a36996fffc5c8105a2f98d8c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-12-28 18:51:20 -0800

    Began adding support for SinglePP cell type annotation.

---
+++ src/run_singlepp.cpp
@@ -0,0 +1,96 @@
+#include <emscripten/bind.h>
+
+#include "singlepp/SinglePP.hpp"
+#include "singlepp/load_references.hpp"
+
+#include "tatami/DenseMatrix.hpp"
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <memory>
+
+class Reference {
+    Reference(
+        size_t nfeat,
+        std::vector<int> rankings,
+        singlepp::Markers marks,
+        const std::vector<std::string>& labs) :
+        markers(std::move(marks));
+    {
+        size_t nprof = labs.size();
+        matrix.reset(new tatami::DenseColumnMatrix<double, int, std::vector<int> >(nfeat, nprof, std::move(rankings)));
+
+        {
+            int counter = 0;
+            labels.reserve(nprof);
+            std::unordered_map<std::string, int> indexed;
+            for (auto l : labs) {
+                auto it = indexed.find(l);
+                if (it != indexed.end()) {
+                    labels.push_back(it->second);
+                } else {
+                    indexed[l] = counter;
+                    labels.push_back(counter);
+                    ++counter;
+                }
+            }
+
+            label_names.resize(indexed.size());
+            for (const auto& i : indexed) {
+                label_names[i.second] = i.first;
+            }
+        }
+    }
+
+    std::shared_ptr<tatami::NumericMatrix> matrix;
+    singlepp::Markers markers;
+    std::vector<int> labels;
+    std::vector<std::string> label_names;
+
+    size_t nlabels() const {
+        return label_names.size();
+    }
+
+    std::string label(int i) const {
+        return label_names[i];
+    }
+};
+
+Reference load_reference(
+    size_t nfeatures,
+    uintptr_t labels_buffer, size_t labels_len,
+    uintptr_t markers_buffer, size_t markers_len,
+    uintptr_t ranking_buffer, size_t ranking_len_
+{ 
+    auto lab = singlepp::load_labels_from_zlib_buffer(reinterpret_cast<unsigned char*>(labels_buffer), labels_len);
+    auto mark = singlepp::load_markers_from_zlib_buffer(reinterpret_cast<unsigned char*>(markers_buffer), markers_len);
+    auto rank = singlepp::load_rankings_from_zlib_buffer(reinterpret_cast<unsigned char*>(rankings_buffer), nfeatures, lab.size());
+    return Reference(nfeatures, std::move(rank), std::move(mark), lab);
+}
+
+void run_singlepp(const NumericMatrix& mat, uintptr_t mat_id, const Reference& ref, uintptr_t ref_id, uintptr_t output) {
+    std::vector<double*> empty(ref.nlabels(), nullptr);
+
+    singlepp::SinglePP runner;
+    runner.run(mat.ptr.get(), 
+        reinterpret_cast<const int*>(mat_id), 
+        ref.matrix.get(), 
+        reinterpret_cast<const int*>(ref_id),
+        ref.markers,
+        reinterpret_cast<int*>(output)
+    );
+    
+    return;
+}
+
+EMSCRIPTEN_BINDINGS(run_singlepp) {
+    emscripten::function("run_singlepp", &run_singlepp);
+
+    emscripten::function("load_reference", &load_reference);
+    
+    emscripten::class_<Reference>("Reference");
+        .function("nlabels", &Reference::nlabels)
+        .function("label", &Reference::label)
+        ;
+}
+
