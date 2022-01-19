95ff1a903968e0df840d2cec0b5ca9b15f717e8a
Aaron Lun <lun.aaron@gene.com>
2022-01-18 16:47:24 -0800

    Allow specification of number of threads at init-time.

--- js/wasm.js
+++ js/wasm.js
@@ -2,17 +2,30 @@ import loadScran from "../build/scran.js";
 
 const cache = {};
 
-export async function initialize(options = {}) {
-    if (! ("module" in cache)) {
-        // TODO: figure out a more portable way of finding the Wasm file.
-        options.locateFile = (x) => import.meta.url.substring(7) + "/../../build/" + x;
-
-        // TODO: add bindings to set the number of threads.
-        // TODO: figure out how to add the thing for service workers.
-        cache.module = await loadScran(options);
+/**
+ * @param {Object} [options] - Optional parameters.
+ * @param {number} [numberOfThreads] - Number of threads to use for calculations.
+ * This will spin up the requested number of Web Workers during module initialization.
+ *
+ * @return 
+ * The Wasm bindings are initialized and `true` is returned.
+ * If the bindings were already initialized (e.g., by a previous call), nothing is done and `false` is returned.
+ */
+export async function initialize({ numberOfThreads = 4 } = {}) {
+    if ("module" in cache) {
+        return false;
     }
 
-    return;
+    let options = {
+        // TODO: figure out a more portable way of finding the Wasm file.
+        locateFile: (x) => import.meta.url.substring(7) + "/../../build/" + x,
+
+        scran_custom_nthreads: numberOfThreads
+    }
+
+    // TODO: figure out how to add the thing for service workers.
+    cache.module = await loadScran(options);
+    return true;
 }
 
 export function call(func) {
@@ -40,6 +53,11 @@ export function buffer() {
     return cache.module.wasmMemory.buffer;
 }
 
+/**
+ * @return All worker threads are terminated and the module is deleted from the cache.
+ * This is useful for cleaning up at the end of the analysis,
+ * otherwise the workers will be shut done ungracefully on program exit.
+ */
 export function terminate() {
     cache.module.PThread.terminateAllThreads();
     delete cache.module;
