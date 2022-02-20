26e07a363a055975a3b1947979fe32b3a8d32a85
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-02-19 21:31:47 -0800

    Added utility to permute features to match LayeredSparseMatrix. Added utility to guess the feature identifier type.

---
+++ js/features.js
@@ -0,0 +1,82 @@
+/**
+ * Permute the genes to match the permutation in a `LayeredSparseMatrix`.
+ *
+ * @param {LayeredSparseMatrix} layered - A `LayeredSparseMatrix` where the rows are permuted for a more memory-efficient storage order.
+ * @param {object} info - A collection of arrays containing per-feature information (e.g., identifiers).
+ * Each array should have length equal to the number of rows in `layered`, but values should refer to features in their original order.
+ *
+ * @return All vectors in `geneInfo` are replaced by their permuted counterparts,
+ * such that entries match the corresponding row in `layered`.
+ */
+export function permuteFeatures(layered, featureInfo) {
+    let perm = layered.permutation();
+    for (const [key, val] of Object.entries(featureInfo)) {
+        let copy = val.slice();
+        perm.forEach((x, i) => {
+            copy[x] = val[i];
+        });
+        featureInfo[key] = copy;
+    }
+    return;
+}
+
+/**
+ * Guess the identity of the features from their names.
+ *
+ * @param {Array} features - Array containing feature identifiers, typically Ensembl IDs or gene symbols.
+ *
+ * @return An object containing the inferred `species`, which can be either `"human"` or `"mouse"`;
+ * and the identifier `type`, which can be either `"ensembl"` or `"symbol"`.
+ * A `confidence` value is reported which defines the percentage of entries in `x` that are consistent with the inferred identity.
+ */
+export function guessFeatures(features) {
+    // Human Ensembl.
+    let human_ens = 0
+    features.forEach(x => {
+        if (x.match(/^ENSG[0-9]{11}$/)) {
+            human_ens++;
+        }
+    });
+
+    // Human symbol; starts with upper case, no lower case, and not an Ensembl of any kind.
+    let human_sym = 0
+    features.forEach(x => {
+        if (x.match(/^[A-Z][^a-z]+$/) && !x.match(/^ENS[A-Z]+[0-9]{11}/)) {
+            human_sym++;
+        }
+    });
+
+    // Mouse Ensembl.
+    let mouse_ens = 0
+    features.forEach(x => {
+        if (x.match(/^ENSMUSG[0-9]{11}$/)) {
+            mouse_ens++;
+        }
+    });
+
+    // Mouse symbol; starts with upper case, but no upper case after that.
+    let mouse_sym = 0
+    features.forEach(x => {
+        if (x.match(/^[A-Z][^A-Z]+$/)) {
+            mouse_sym++;
+        }
+    });
+
+    // Who's the highest?
+    let output = [
+        { "species": "human", "type": "ensembl", "confidence": human_ens},
+        { "species": "human", "type": "symbol", "confidence": human_sym},
+        { "species": "mouse", "type": "ensembl", "confidence": mouse_ens},
+        { "species": "mouse", "type": "symbol", "confidence": mouse_sym}
+    ];
+
+    let highest = output[0];
+    for (var i = 1; i < output.length; i++) {
+        if (output[i].confidence > highest.confidence) {
+            highest = output[i];
+        }
+    }
+
+    highest.confidence /= features.length;
+    return highest;
+}
