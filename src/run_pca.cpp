97927df6bac91e51d512c459b174fe68a7274a02
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-03-20 02:08:43 -0700

    Added C++ bindings for the BlockedPCA function.

--- src/run_pca.cpp
+++ src/run_pca.cpp
@@ -2,6 +2,8 @@
 
 #include "NumericMatrix.h"
 #include "scran/dimensionality_reduction/RunPCA.hpp"
+#include "scran/dimensionality_reduction/MultiBatchPCA.hpp"
+#include "scran/dimensionality_reduction/BlockedPCA.hpp"
 
 #include <vector>
 #include <cmath>
@@ -14,15 +16,14 @@
  */
 
 /**
- * @brief Javascript-visible wrapper around `scran::RunPCA::Results`.
+ * @brief Template for Javascript-visible wrappers around `scran::RunPCA::Results` and relatives.
  */
-struct RunPCA_Results {
+template<class Store>
+struct PCA_Results {
     /**
      * @cond
      */
-    typedef scran::RunPCA::Results Store;
-
-    RunPCA_Results(Store s) : store(std::move(s)) {}
+    PCA_Results(Store s) : store(std::move(s)) {}
 
     Store store;
     /**
@@ -67,6 +68,25 @@ struct RunPCA_Results {
 };
 
 /**
+ * Realization of `PCA_Results` to wrap `scran::RunPCA` output.
+ */
+using RunPCA_Results = PCA_Results<scran::RunPCA::Results>;
+
+const uint8_t* precheck_inputs(int number, size_t NC, bool use_subset, uintptr_t subset) {
+    if (number < 1) {
+        throw std::runtime_error("requested number of PCs should be positive");
+    }
+    if (NC < number) {
+        throw std::runtime_error("fewer cells than the requested number of PCs");
+    }
+    const uint8_t* subptr = NULL;
+    if (use_subset) {
+        subptr = reinterpret_cast<const uint8_t*>(subset);
+    }
+    return subptr;
+}
+
+/**
  * Perform a principal components analysis to obtain per-cell coordinates in low-dimensional space.
  *
  * @param mat The input log-expression matrix, with features in rows and cells in columns.
@@ -86,20 +106,10 @@ RunPCA_Results run_pca(const NumericMatrix& mat, int number, bool use_subset, ui
     auto NR = ptr->nrow();
     auto NC = ptr->ncol();
 
-    if (number < 1) {
-        throw std::runtime_error("requested number of PCs should be positive");
-    }
-    if (NC < number) {
-        throw std::runtime_error("fewer cells than the requested number of PCs");
-    }
+    auto subptr = precheck_inputs(number, NC, use_subset, subset);
 
     scran::RunPCA pca;
     pca.set_rank(number).set_scale(scale);
-
-    const uint8_t* subptr = NULL;
-    if (use_subset) {
-        subptr = reinterpret_cast<const uint8_t*>(subset);
-    }
     auto result = pca.run(ptr.get(), subptr);
 
     // Transposing PCs to get the right orientation.
@@ -109,10 +119,53 @@ RunPCA_Results run_pca(const NumericMatrix& mat, int number, bool use_subset, ui
 }
 
 /**
+ * Realization of `PCA_Results` to wrap `scran::BlockedPCA` output.
+ */
+using BlockedPCA_Results = PCA_Results<scran::BlockedPCA::Results>;
+
+/**
+ * Perform a principal components analysis after blocking on a factor across the cells.
+ * This is equivalent to performing a PCA on the residuals after regressing out the factor.
+ *
+ * @param mat The input log-expression matrix, with features in rows and cells in columns.
+ * @param number Number of PCs to obtain.
+ * Must be less than the smaller dimension of `mat`.
+ * @param use_subset Whether to subset the matrix to features of interest in `subset`.
+ * @param[in] subset Offset to an input array of `uint8_t`s of length `mat.nrow()`,
+ * indicating which features should be used for the PCA.
+ * Only used if `use_subset = true`.
+ * @param scale Whether to standardize rows in `mat` to unit variance.
+ * If `true`, all rows in `mat` are assumed to have non-zero variance.
+ * @param[in] blocks Offset to an array of `int32_t`s with `ncells` elements, containing the block assignment for each cell.
+ * Block IDs should be consecutive and 0-based.
+ *
+ * @return A `RunPCA_Results` object is returned containing the PCA results.
+ */
+BlockedPCA_Results run_blocked_pca(const NumericMatrix& mat, int number, bool use_subset, uintptr_t subset, bool scale, uintptr_t blocks) {
+    auto ptr = mat.ptr;
+    auto NR = ptr->nrow();
+    auto NC = ptr->ncol();
+
+    auto subptr = precheck_inputs(number, NC, use_subset, subset);
+    auto bptr = reinterpret_cast<const int32_t*>(blocks);
+
+    scran::BlockedPCA pca;
+    pca.set_rank(number).set_scale(scale);
+    auto result = pca.run(ptr.get(), bptr, subptr);
+
+    // Transposing PCs to get the right orientation.
+    result.pcs.adjointInPlace();
+
+    return BlockedPCA_Results(std::move(result)); 
+}
+
+/**
  * @cond
  */
 EMSCRIPTEN_BINDINGS(run_pca) {
     emscripten::function("run_pca", &run_pca);
+
+    emscripten::function("run_pca", &run_blocked_pca);
 
     emscripten::class_<RunPCA_Results>("RunPCA_Results")
         .function("pcs", &RunPCA_Results::pcs)
@@ -120,6 +173,14 @@ EMSCRIPTEN_BINDINGS(run_pca) {
         .function("total_variance", &RunPCA_Results::total_variance)
         .function("num_cells", &RunPCA_Results::num_cells)
         .function("num_pcs", &RunPCA_Results::num_pcs)
+        ;
+
+    emscripten::class_<BlockedPCA_Results>("BlockedPCA_Results")
+        .function("pcs", &BlockedPCA_Results::pcs)
+        .function("variance_explained", &BlockedPCA_Results::variance_explained)
+        .function("total_variance", &BlockedPCA_Results::total_variance)
+        .function("num_cells", &BlockedPCA_Results::num_cells)
+        .function("num_pcs", &BlockedPCA_Results::num_pcs)
         ;
 }
 /**
