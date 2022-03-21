04f9ee76a4d3efdabab36ae4c5fee9509605efba
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2022-03-20 17:58:53 -0700

    Support multiple flavors of blocking in runPCA. (#43)

--- tests/runPCA.test.js
+++ tests/runPCA.test.js
@@ -1,5 +1,6 @@
 import * as scran from "../js/index.js";
 import * as simulate from "./simulate.js";
+import * as compare from "./compare.js";
 
 beforeAll(async () => { await scran.initialize({ localFile: true }) });
 afterAll(async () => { await scran.terminate() });
@@ -37,4 +38,36 @@ test("PCA works as expected with feature subsetting", () => {
     // Mopping up.
     mat.free();
     pca.free();
+});
+
+test("PCA works as expected with blocking", () => {
+    var ngenes = 1000;
+    var ncells = 100;
+    var mat = simulate.simulateMatrix(ngenes, ncells);
+
+    var feat = new Array(ngenes);
+    for (var i = 0; i < ngenes; i++) {
+        feat[i] = Math.random() < 0.05;
+    }
+
+    var block = new Int32Array(ncells);
+    var nblocks = 4;
+    for (var i = 0; i < ncells; i++) {
+        block[i] = Math.floor(Math.random() * nblocks);
+    }
+    for (var j = 0; j < nblocks; j++) {
+        block[j] = j;
+    }
+
+    var pca = scran.runPCA(mat, { features: feat, numberOfPCs: 15 });
+
+    var blocked = scran.runPCA(mat, { features: feat, numberOfPCs: 15, block: block });
+    expect(pca.numberOfPCs()).toBe(blocked.numberOfPCs());
+    expect(pca.numberOfCells()).toBe(blocked.numberOfCells());
+    expect(compare.equalFloatArrays(pca.principalComponents(), blocked.principalComponents())).toBe(false);
+
+    var weighted = scran.runPCA(mat, { features: feat, numberOfPCs: 15, block: block, blockMethod: "weight" });
+    expect(pca.numberOfPCs()).toBe(weighted.numberOfPCs());
+    expect(pca.numberOfCells()).toBe(weighted.numberOfCells());
+    expect(compare.equalFloatArrays(pca.principalComponents(), weighted.principalComponents())).toBe(false);
 });
