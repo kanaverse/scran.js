f24dcc1f070ab5c3201b089d549c1fa5db46303f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-03-21 23:03:49 +0000

    Deploying to gh-pages from @ jkanche/scran.js@4a02cb017a73f0a673071d601d9eebaae7b11c1b 🚀

--- docs/wasm/search/files_0.js
+++ docs/wasm/search/files_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['cluster_5fkmeans_2ecpp_0',['cluster_kmeans.cpp',['../cluster__kmeans_8cpp.html',1,'']]],
-  ['cluster_5fsnn_5fgraph_2ecpp_1',['cluster_snn_graph.cpp',['../cluster__snn__graph_8cpp.html',1,'']]]
+  ['cbind_2ecpp_0',['cbind.cpp',['../cbind_8cpp.html',1,'']]],
+  ['cluster_5fkmeans_2ecpp_1',['cluster_kmeans.cpp',['../cluster__kmeans_8cpp.html',1,'']]],
+  ['cluster_5fsnn_5fgraph_2ecpp_2',['cluster_snn_graph.cpp',['../cluster__snn__graph_8cpp.html',1,'']]]
 ];
--- docs/wasm/search/functions_2.js
+++ docs/wasm/search/functions_2.js
@@ -1,14 +1,16 @@
 var searchData=
 [
-  ['centers_0',['centers',['../structClusterKmeans__Result.html#a886dc0a016f6f79793e959641469e8e6',1,'ClusterKmeans_Result']]],
-  ['cluster_5fkmeans_1',['cluster_kmeans',['../cluster__kmeans_8cpp.html#a9e7eacdb96c9c9814bc378e8a6e4ac51',1,'cluster_kmeans.cpp']]],
-  ['cluster_5fsizes_2',['cluster_sizes',['../structClusterKmeans__Result.html#ad6b03aa4750f2ff2b20ac5ce5b635493',1,'ClusterKmeans_Result']]],
-  ['cluster_5fsnn_5fgraph_3',['cluster_snn_graph',['../cluster__snn__graph_8cpp.html#a4a3639798602da8d725c245d18a5c52d',1,'cluster_snn_graph.cpp']]],
-  ['clusters_4',['clusters',['../structClusterKmeans__Result.html#a41d6c9c8579153b7611274f83e0ae18a',1,'ClusterKmeans_Result']]],
-  ['cohen_5',['cohen',['../structScoreMarkers__Results.html#a77ce6eb68fd05cff4a5218e4a6fb549c',1,'ScoreMarkers_Results']]],
-  ['column_6',['column',['../structNumericMatrix.html#a3501cce20e99fa6d4a1ea61c8878c141',1,'NumericMatrix']]],
-  ['convert_5farray_5fof_5foffsets_7',['convert_array_of_offsets',['../utils_8h.html#af054ae3b1991fa1a539d347086f00114',1,'utils.h']]],
-  ['create_5fhdf5_5fdataset_8',['create_hdf5_dataset',['../hdf5__utils_8cpp.html#afc422ca924c7427d4a0f6b4d13fd64ea',1,'hdf5_utils.cpp']]],
-  ['create_5fhdf5_5ffile_9',['create_hdf5_file',['../hdf5__utils_8cpp.html#ad9865cce9a0309fa8ce7648a3f56c4d0',1,'hdf5_utils.cpp']]],
-  ['create_5fhdf5_5fgroup_10',['create_hdf5_group',['../hdf5__utils_8cpp.html#a979d6d36d63ecc0f399a56b1f6e0281b',1,'hdf5_utils.cpp']]]
+  ['cbind_0',['cbind',['../cbind_8cpp.html#a575ae6f30756ee175b6f5bcb8c544b29',1,'cbind.cpp']]],
+  ['cbind_5fwith_5frownames_1',['cbind_with_rownames',['../cbind_8cpp.html#afaaf69c3fd79a2cba53dcc38e7b92d7b',1,'cbind.cpp']]],
+  ['centers_2',['centers',['../structClusterKmeans__Result.html#a886dc0a016f6f79793e959641469e8e6',1,'ClusterKmeans_Result']]],
+  ['cluster_5fkmeans_3',['cluster_kmeans',['../cluster__kmeans_8cpp.html#a9e7eacdb96c9c9814bc378e8a6e4ac51',1,'cluster_kmeans.cpp']]],
+  ['cluster_5fsizes_4',['cluster_sizes',['../structClusterKmeans__Result.html#ad6b03aa4750f2ff2b20ac5ce5b635493',1,'ClusterKmeans_Result']]],
+  ['cluster_5fsnn_5fgraph_5',['cluster_snn_graph',['../cluster__snn__graph_8cpp.html#a4a3639798602da8d725c245d18a5c52d',1,'cluster_snn_graph.cpp']]],
+  ['clusters_6',['clusters',['../structClusterKmeans__Result.html#a41d6c9c8579153b7611274f83e0ae18a',1,'ClusterKmeans_Result']]],
+  ['cohen_7',['cohen',['../structScoreMarkers__Results.html#a77ce6eb68fd05cff4a5218e4a6fb549c',1,'ScoreMarkers_Results']]],
+  ['column_8',['column',['../structNumericMatrix.html#a3501cce20e99fa6d4a1ea61c8878c141',1,'NumericMatrix']]],
+  ['convert_5farray_5fof_5foffsets_9',['convert_array_of_offsets',['../utils_8h.html#af054ae3b1991fa1a539d347086f00114',1,'utils.h']]],
+  ['create_5fhdf5_5fdataset_10',['create_hdf5_dataset',['../hdf5__utils_8cpp.html#afc422ca924c7427d4a0f6b4d13fd64ea',1,'hdf5_utils.cpp']]],
+  ['create_5fhdf5_5ffile_11',['create_hdf5_file',['../hdf5__utils_8cpp.html#ad9865cce9a0309fa8ce7648a3f56c4d0',1,'hdf5_utils.cpp']]],
+  ['create_5fhdf5_5fgroup_12',['create_hdf5_group',['../hdf5__utils_8cpp.html#a979d6d36d63ecc0f399a56b1f6e0281b',1,'hdf5_utils.cpp']]]
 ];
