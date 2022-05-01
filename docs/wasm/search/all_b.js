a95cd31d3d1609732c8875ed196128a08a2b6d45
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-05-01 02:56:48 +0000

    Deploying to gh-pages from @ jkanche/scran.js@6a18fed24abce69639d61665d9514abab1e0cfd6 🚀

--- docs/wasm/search/all_b.js
+++ docs/wasm/search/all_b.js
@@ -1,12 +1,14 @@
 var searchData=
 [
   ['means_0',['means',['../structModelGeneVar__Results.html#ade61a7f6697f80e1d22e1af5f081d6de',1,'ModelGeneVar_Results::means()'],['../structScoreMarkers__Results.html#ace8ae48f96297598475fd5bfbfebeec6',1,'ScoreMarkers_Results::means()']]],
-  ['membership_1',['membership',['../structClusterSNNGraphMultiLevel__Result.html#a40d182a1859811ca6dbb85c0bb98559a',1,'ClusterSNNGraphMultiLevel_Result']]],
-  ['mnn_5fcorrect_2',['mnn_correct',['../mnn__correct_8cpp.html#a0c385ed30dea27516266d0e9b387275a',1,'mnn_correct.cpp']]],
-  ['mnn_5fcorrect_2ecpp_3',['mnn_correct.cpp',['../mnn__correct_8cpp.html',1,'']]],
-  ['model_5fgene_5fvar_4',['model_gene_var',['../model__gene__var_8cpp.html#a293da11d683b41f396389ef742ac8fd5',1,'model_gene_var.cpp']]],
-  ['model_5fgene_5fvar_2ecpp_5',['model_gene_var.cpp',['../model__gene__var_8cpp.html',1,'']]],
-  ['modelgenevar_5fresults_6',['ModelGeneVar_Results',['../structModelGeneVar__Results.html',1,'']]],
-  ['modularity_7',['modularity',['../structClusterSNNGraphMultiLevel__Result.html#a0ede340456cc20f3a82f1009d4fe898d',1,'ClusterSNNGraphMultiLevel_Result']]],
-  ['multibatchpca_5fresults_8',['MultiBatchPCA_Results',['../run__pca_8cpp.html#a214d4b43c591cbde727307a5c0f46fee',1,'run_pca.cpp']]]
+  ['median_5fsize_5ffactors_1',['median_size_factors',['../median__size__factors_8cpp.html#a2db14fd8508a2acce5484aeaa60c20e3',1,'median_size_factors.cpp']]],
+  ['median_5fsize_5ffactors_2ecpp_2',['median_size_factors.cpp',['../median__size__factors_8cpp.html',1,'']]],
+  ['membership_3',['membership',['../structClusterSNNGraphMultiLevel__Result.html#a40d182a1859811ca6dbb85c0bb98559a',1,'ClusterSNNGraphMultiLevel_Result']]],
+  ['mnn_5fcorrect_4',['mnn_correct',['../mnn__correct_8cpp.html#a0c385ed30dea27516266d0e9b387275a',1,'mnn_correct.cpp']]],
+  ['mnn_5fcorrect_2ecpp_5',['mnn_correct.cpp',['../mnn__correct_8cpp.html',1,'']]],
+  ['model_5fgene_5fvar_6',['model_gene_var',['../model__gene__var_8cpp.html#a293da11d683b41f396389ef742ac8fd5',1,'model_gene_var.cpp']]],
+  ['model_5fgene_5fvar_2ecpp_7',['model_gene_var.cpp',['../model__gene__var_8cpp.html',1,'']]],
+  ['modelgenevar_5fresults_8',['ModelGeneVar_Results',['../structModelGeneVar__Results.html',1,'']]],
+  ['modularity_9',['modularity',['../structClusterSNNGraphMultiLevel__Result.html#a0ede340456cc20f3a82f1009d4fe898d',1,'ClusterSNNGraphMultiLevel_Result']]],
+  ['multibatchpca_5fresults_10',['MultiBatchPCA_Results',['../run__pca_8cpp.html#a214d4b43c591cbde727307a5c0f46fee',1,'run_pca.cpp']]]
 ];
--- docs/wasm/search/functions_b.js
+++ docs/wasm/search/functions_b.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['means_0',['means',['../structModelGeneVar__Results.html#ade61a7f6697f80e1d22e1af5f081d6de',1,'ModelGeneVar_Results::means()'],['../structScoreMarkers__Results.html#ace8ae48f96297598475fd5bfbfebeec6',1,'ScoreMarkers_Results::means()']]],
-  ['membership_1',['membership',['../structClusterSNNGraphMultiLevel__Result.html#a40d182a1859811ca6dbb85c0bb98559a',1,'ClusterSNNGraphMultiLevel_Result']]],
-  ['mnn_5fcorrect_2',['mnn_correct',['../mnn__correct_8cpp.html#a0c385ed30dea27516266d0e9b387275a',1,'mnn_correct.cpp']]],
-  ['model_5fgene_5fvar_3',['model_gene_var',['../model__gene__var_8cpp.html#a293da11d683b41f396389ef742ac8fd5',1,'model_gene_var.cpp']]],
-  ['modularity_4',['modularity',['../structClusterSNNGraphMultiLevel__Result.html#a0ede340456cc20f3a82f1009d4fe898d',1,'ClusterSNNGraphMultiLevel_Result']]]
+  ['median_5fsize_5ffactors_1',['median_size_factors',['../median__size__factors_8cpp.html#a2db14fd8508a2acce5484aeaa60c20e3',1,'median_size_factors.cpp']]],
+  ['membership_2',['membership',['../structClusterSNNGraphMultiLevel__Result.html#a40d182a1859811ca6dbb85c0bb98559a',1,'ClusterSNNGraphMultiLevel_Result']]],
+  ['mnn_5fcorrect_3',['mnn_correct',['../mnn__correct_8cpp.html#a0c385ed30dea27516266d0e9b387275a',1,'mnn_correct.cpp']]],
+  ['model_5fgene_5fvar_4',['model_gene_var',['../model__gene__var_8cpp.html#a293da11d683b41f396389ef742ac8fd5',1,'model_gene_var.cpp']]],
+  ['modularity_5',['modularity',['../structClusterSNNGraphMultiLevel__Result.html#a0ede340456cc20f3a82f1009d4fe898d',1,'ClusterSNNGraphMultiLevel_Result']]]
 ];
