42489e81358416ed159844301374271b94fe286e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-05-01 06:58:36 +0000

    Deploying to gh-pages from @ jkanche/scran.js@23ce1530e15a63a8c65bc77708d355a1b2b00cba 🚀

--- docs/wasm/search/all_6.js
+++ docs/wasm/search/all_6.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['get_5ferror_5fmessage_0',['get_error_message',['../get__error__message_8cpp.html#ac2295559558e9b32f9b9620af3956d1e',1,'get_error_message.cpp']]],
   ['get_5ferror_5fmessage_2ecpp_1',['get_error_message.cpp',['../get__error__message_8cpp.html',1,'']]],
-  ['guess_5fhdf5_5ftype_2',['guess_hdf5_type',['../hdf5__utils_8cpp.html#a946eabb82850d4e93fbd51ea73b806b4',1,'hdf5_utils.cpp']]]
+  ['grouped_5fsize_5ffactors_2',['grouped_size_factors',['../grouped__size__factors_8cpp.html#ab6c8127e3063a6d6e6d9318b28907d78',1,'grouped_size_factors.cpp']]],
+  ['grouped_5fsize_5ffactors_2ecpp_3',['grouped_size_factors.cpp',['../grouped__size__factors_8cpp.html',1,'']]],
+  ['guess_5fhdf5_5ftype_4',['guess_hdf5_type',['../hdf5__utils_8cpp.html#a946eabb82850d4e93fbd51ea73b806b4',1,'hdf5_utils.cpp']]]
 ];
