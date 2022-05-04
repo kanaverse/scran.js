00e3878d500e6f07616df330f578ff2b6d5260be
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-05-04 16:26:49 +0000

    Deploying to gh-pages from @ jkanche/scran.js@ea26d0989ee3bcf0a4abcf3ef4ae41232002632c 🚀

--- docs/wasm/search/functions_e.js
+++ docs/wasm/search/functions_e.js
@@ -3,13 +3,14 @@ var searchData=
   ['randomize_5ftsne_5fstart_0',['randomize_tsne_start',['../run__tsne_8cpp.html#aa2655f4139c7fc61647f49a204635adc',1,'run_tsne.cpp']]],
   ['read_5fhdf5_5fmatrix_1',['read_hdf5_matrix',['../read__hdf5__matrix_8cpp.html#a3148acc69d25b26ad959758ccfd7510b',1,'read_hdf5_matrix.cpp']]],
   ['read_5fmatrix_5fmarket_2',['read_matrix_market',['../read__matrix__market_8cpp.html#ae97d4b17f48f6d25070f7e6dc91e20b3',1,'read_matrix_market.cpp']]],
-  ['residuals_3',['residuals',['../structModelGeneVar__Results.html#afb80eb6a019e26e128e5114dadb0c42e',1,'ModelGeneVar_Results']]],
-  ['row_4',['row',['../structNumericMatrix.html#a8b56c2bd09fea326c0c28a8a0add6a6e',1,'NumericMatrix']]],
-  ['row_5fsubset_5',['row_subset',['../subset_8cpp.html#a4f1bf0e95603c097b08a4ea353474036',1,'subset.cpp']]],
-  ['run_5fblocked_5fpca_6',['run_blocked_pca',['../run__pca_8cpp.html#adaf520ade752f6e294601a2ec022d9c4',1,'run_pca.cpp']]],
-  ['run_5fmultibatch_5fpca_7',['run_multibatch_pca',['../run__pca_8cpp.html#a31056c30acd971ec17175ec47c759958',1,'run_pca.cpp']]],
-  ['run_5fpca_8',['run_pca',['../run__pca_8cpp.html#a02ea0870e6d0c9f18277d43aa1f9e028',1,'run_pca.cpp']]],
-  ['run_5fsinglepp_9',['run_singlepp',['../run__singlepp_8cpp.html#a1262e14f9a85fad9c4e04290f48e1299',1,'run_singlepp.cpp']]],
-  ['run_5ftsne_10',['run_tsne',['../run__tsne_8cpp.html#ad29d8d3b48e3165dc7d8f0476a46119d',1,'run_tsne.cpp']]],
-  ['run_5fumap_11',['run_umap',['../run__umap_8cpp.html#aed48c871b2d43d7b2a6aeacba599451f',1,'run_umap.cpp']]]
+  ['reorganized_3',['reorganized',['../structNumericMatrix.html#a87961b02a42a22f10fbc23992449334b',1,'NumericMatrix']]],
+  ['residuals_4',['residuals',['../structModelGeneVar__Results.html#afb80eb6a019e26e128e5114dadb0c42e',1,'ModelGeneVar_Results']]],
+  ['row_5',['row',['../structNumericMatrix.html#a8b56c2bd09fea326c0c28a8a0add6a6e',1,'NumericMatrix']]],
+  ['row_5fsubset_6',['row_subset',['../subset_8cpp.html#a4f1bf0e95603c097b08a4ea353474036',1,'subset.cpp']]],
+  ['run_5fblocked_5fpca_7',['run_blocked_pca',['../run__pca_8cpp.html#adaf520ade752f6e294601a2ec022d9c4',1,'run_pca.cpp']]],
+  ['run_5fmultibatch_5fpca_8',['run_multibatch_pca',['../run__pca_8cpp.html#a31056c30acd971ec17175ec47c759958',1,'run_pca.cpp']]],
+  ['run_5fpca_9',['run_pca',['../run__pca_8cpp.html#a02ea0870e6d0c9f18277d43aa1f9e028',1,'run_pca.cpp']]],
+  ['run_5fsinglepp_10',['run_singlepp',['../run__singlepp_8cpp.html#a1262e14f9a85fad9c4e04290f48e1299',1,'run_singlepp.cpp']]],
+  ['run_5ftsne_11',['run_tsne',['../run__tsne_8cpp.html#ad29d8d3b48e3165dc7d8f0476a46119d',1,'run_tsne.cpp']]],
+  ['run_5fumap_12',['run_umap',['../run__umap_8cpp.html#aed48c871b2d43d7b2a6aeacba599451f',1,'run_umap.cpp']]]
 ];
