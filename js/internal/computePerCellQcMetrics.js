db027e57f4a5d9f9517c55134aec64dfae1fad3f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-05-12 21:42:51 -0700

    Add bindings for the ADT-based QC functions.

---
+++ js/internal/computePerCellQcMetrics.js
@@ -0,0 +1,57 @@
+import * as wasm from "../wasm.js";
+import * as utils from "../utils.js"; 
+import * as wa from "wasmarrays.js";
+
+export function computePerCellQcMetrics(x, subsets, run, create) {
+    var output;
+    var raw;
+
+    try {
+        if (subsets instanceof wa.Uint8WasmArray) {
+            let nsubsets = Math.round(subsets.length / x.numberOfRows());
+            if (nsubsets * x.numberOfRows() != subsets.length) {
+                throw new Error("length of 'subsets' should be a multiple of the matrix rows");
+            }
+
+            // This will either create a cheap view, or it'll clone
+            // 'subsets' into the appropriate memory space.
+            let converted = utils.wasmifyArray(subsets, "Uint8WasmArray");
+            try {
+                let ptr = subsets.offset;
+                raw = run(x.matrix, nsubsets, ptr);
+            } finally {
+                converted.free();
+            }
+
+        } else if (subsets instanceof Array) {
+            let tmp = utils.createUint8WasmArray(x.numberOfRows() * subsets.length);
+            try {
+                let offset = 0;
+                for (var i = 0; i < subsets.length; i++) {
+                    let current = subsets[i];
+                    if (current.length != x.numberOfRows()) {
+                        throw new Error("length of each array in 'subsets' should be equal to the matrix rows");
+                    }
+                    tmp.array().set(current, offset);
+                    offset += current.length;
+                }
+                raw = run(x.matrix, subsets.length, tmp.offset);
+            } finally {
+                tmp.free();
+            }
+
+        } else if (subsets === null) {
+            raw = run(x.matrix, 0, 0);
+
+        } else {
+            throw new Error("'subsets' should be an Array or Uint8WasmArray");
+        }
+
+        output = create(raw);
+    } catch (e) {
+        utils.free(raw);
+        throw e;
+    }
+
+    return output;
+}
---
+++ src/per_cell_adt_qc_metrics.cpp
@@ -0,0 +1,29 @@
+#include <emscripten/bind.h>
+
+#include "NumericMatrix.h"
+#include "utils.h"
+#include "PerCellAdtQcMetrics_Results.h"
+
+#include "scran/quality_control/PerCellAdtQcMetrics.hpp"
+
+#include <vector>
+#include <cstdint>
+#include <cmath>
+
+PerCellAdtQcMetrics_Results per_cell_adt_qc_metrics(const NumericMatrix& mat, int nsubsets, uintptr_t subsets) {
+    scran::PerCellAdtQcMetrics qc;
+    auto store = qc.run(mat.ptr.get(), extract_column_pointers<const uint8_t*>(subsets, mat.nrow(), nsubsets));
+    return PerCellAdtQcMetrics_Results(std::move(store));
+}
+
+EMSCRIPTEN_BINDINGS(per_cell_qc_metrics) {
+    emscripten::function("per_cell_adt_qc_metrics", &per_cell_adt_qc_metrics);
+
+    emscripten::class_<PerCellAdtQcMetrics_Results>("PerCellAdtQcMetrics_Results")
+        .constructor<int, int>()
+        .function("sums", &PerCellAdtQcMetrics_Results::sums)
+        .function("detected", &PerCellAdtQcMetrics_Results::detected)
+        .function("subset_totals", &PerCellAdtQcMetrics_Results::subset_totals)
+        .function("num_subsets", &PerCellAdtQcMetrics_Results::num_subsets)
+        ;
+}
