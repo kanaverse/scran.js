c91b72c164de57c77fd92f6667cf59399a1db37a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-05-14 18:56:36 +0000

    Deploying to gh-pages from @ jkanche/scran.js@7632538c0cf3eb4e0e8dacab64ea5e2423f600c2 🚀

--- docs/wasm/search/functions_4.js
+++ docs/wasm/search/functions_4.js
@@ -1,9 +1,7 @@
 var searchData=
 [
   ['em_5fjs_0',['EM_JS',['../parallel_8cpp.html#acac72901eb433808b509bbfe1de2b9e1',1,'parallel.cpp']]],
-  ['emscripten_5fbindings_1',['EMSCRIPTEN_BINDINGS',['../per__cell__adt__qc__filters_8cpp.html#add5fba1ab6b130e634a39124585c4df7',1,'EMSCRIPTEN_BINDINGS(per_cell_adt_qc_filters):&#160;per_cell_adt_qc_filters.cpp'],['../per__cell__adt__qc__metrics_8cpp.html#a6ef638d010a5eada7076556002f81cfe',1,'EMSCRIPTEN_BINDINGS(per_cell_qc_metrics):&#160;per_cell_adt_qc_metrics.cpp'],['../scale__by__neighbors_8cpp.html#a2c9577a2153ab53b50d96cbb1e021450',1,'EMSCRIPTEN_BINDINGS(scale_by_neighbors):&#160;scale_by_neighbors.cpp']]],
+  ['emscripten_5fbindings_1',['EMSCRIPTEN_BINDINGS',['../per__cell__adt__qc__filters_8cpp.html#add5fba1ab6b130e634a39124585c4df7',1,'EMSCRIPTEN_BINDINGS(per_cell_adt_qc_filters):&#160;per_cell_adt_qc_filters.cpp'],['../per__cell__adt__qc__metrics_8cpp.html#a6ef638d010a5eada7076556002f81cfe',1,'EMSCRIPTEN_BINDINGS(per_cell_qc_metrics):&#160;per_cell_adt_qc_metrics.cpp'],['../per__cell__qc__metrics_8cpp.html#a6ef638d010a5eada7076556002f81cfe',1,'EMSCRIPTEN_BINDINGS(per_cell_qc_metrics):&#160;per_cell_qc_metrics.cpp'],['../scale__by__neighbors_8cpp.html#a2c9577a2153ab53b50d96cbb1e021450',1,'EMSCRIPTEN_BINDINGS(scale_by_neighbors):&#160;scale_by_neighbors.cpp']]],
   ['end_2',['end',['../classJSVector.html#a674e144348cafa9c1b3b311c2058e4e0',1,'JSVector']]],
-  ['epoch_3',['epoch',['../structUmapStatus.html#aa7d012c8f11d269269b630007b9ec277',1,'UmapStatus']]],
-  ['extract_5fcolumn_5fpointers_4',['extract_column_pointers',['../utils_8h.html#a3a43aae089fa138a49907e68c304c6ac',1,'utils.h']]],
-  ['extract_5fcolumn_5fpointers_5fblocked_5',['extract_column_pointers_blocked',['../utils_8h.html#a02d5e780d6999045dc209bfcd583bd3b',1,'utils.h']]]
+  ['epoch_3',['epoch',['../structUmapStatus.html#aa7d012c8f11d269269b630007b9ec277',1,'UmapStatus']]]
 ];
