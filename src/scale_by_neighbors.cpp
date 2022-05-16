ef40d553b4915943dafd9a547a72937b515f131a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-05-16 14:29:27 -0700

    Simplified the C++ code for scaleByNeighbors.

--- src/scale_by_neighbors.cpp
+++ src/scale_by_neighbors.cpp
@@ -16,26 +16,7 @@ void scale_by_neighbors(int ncells, int nembed, uintptr_t embeddings, uintptr_t 
         distances[e] = runner.compute_distance((index_ptrs[e]->search).get());
     }
 
-    // Use the first entry with a non-zero RMSD as the reference.
-    int ref = -1;
-    for (int e = 0; e < nembed; ++e) {
-        if (distances[e].second) {
-            ref = e;
-            break;
-        }
-    }
-
-    std::vector<double> scaling(nembed);
-    if (ref >= 0) {
-        for (int e = 0; e < nembed; ++e) {
-            if (e == ref) {
-                scaling[e] = 1;
-            } else {
-                scaling[e] = runner.compute_scale(distances[ref], distances[e]);
-            }
-        }
-    }
-
+    auto scaling = scran::ScaleByNeighbors::compute_scale(distances);
     if (use_weights) {
         auto weight_ptr = reinterpret_cast<const double*>(weights);
         for (int e = 0; e < nembed; ++e) {
@@ -44,31 +25,15 @@ void scale_by_neighbors(int ncells, int nembed, uintptr_t embeddings, uintptr_t 
     }
 
     // Interleaving the scaled embeddings.
-    size_t ntotal = 0;
+    std::vector<int> ndims(nembed);
     for (int e = 0; e < nembed; ++e) {
         const auto& index = (index_ptrs[e]->search);
-        ntotal += index->ndim();
+        ndims[e] = index->ndim();
     }
 
     auto embed_ptrs = convert_array_of_offsets<const double*>(nembed, embeddings);
     auto out_ptr = reinterpret_cast<double*>(combined);
-    size_t offset = 0;
-
-    for (int e = 0; e < nembed; ++e) {
-        const auto& index = (index_ptrs[e]->search);
-        size_t curdim = index->ndim();
-        auto input = embed_ptrs[e];
-        auto output = out_ptr + offset;
-        auto s = scaling[e];
-
-        for (size_t c = 0; c < ncells; ++c, input += curdim, output += ntotal) {
-            for (size_t d = 0; d < curdim; ++d) {
-                output[d] = input[d] * s;
-            }
-        }
-
-        offset += curdim;
-    }
+    scran::ScaleByNeighbors::combine_scaled_embeddings(ndims, ncells, embed_ptrs, scaling, out_ptr);
 }
 
 EMSCRIPTEN_BINDINGS(scale_by_neighbors) {
