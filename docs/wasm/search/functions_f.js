5de8d216043c2c67598ecead2ea145e1a4d4d219
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-05-17 04:06:24 +0000

    Deploying to gh-pages from @ jkanche/scran.js@a4fe962ec328ee52c5fb1c7e8bce52322e896305 🚀

--- docs/wasm/search/functions_f.js
+++ docs/wasm/search/functions_f.js
@@ -1,14 +1,16 @@
 var searchData=
 [
-  ['scale_5fby_5fneighbors_0',['scale_by_neighbors',['../scale__by__neighbors_8cpp.html#a03633af4d5205f92ca7c98082ecdaf8c',1,'scale_by_neighbors.cpp']]],
-  ['score_5fmarkers_1',['score_markers',['../score__markers_8cpp.html#a96d5665269882aa23e30d033cd659973',1,'score_markers.cpp']]],
-  ['serialize_2',['serialize',['../structNeighborResults.html#af019e13656c3b08923316f07870c6729',1,'NeighborResults']]],
-  ['shape_3',['shape',['../structH5DataSetDetails.html#a01773edafddba4f46022427f8445b64f',1,'H5DataSetDetails::shape()'],['../structLoadedH5DataSet.html#a5e1e0adddab2e5ddac1605c4715941da',1,'LoadedH5DataSet::shape()']]],
-  ['shared_5ffeatures_4',['shared_features',['../classBuiltSinglePPReference.html#aa0e27c099cc5160b31d680d98591f6df',1,'BuiltSinglePPReference']]],
-  ['size_5',['size',['../classJSVector.html#a29628665d2589799afc3389ef06991aa',1,'JSVector::size()'],['../structNeighborResults.html#a210be834f21e0f20f5b373cf5489e5bd',1,'NeighborResults::size()']]],
-  ['sparse_6',['sparse',['../structNumericMatrix.html#a0972e2d156b0e63b625ee6565f6cea8c',1,'NumericMatrix']]],
-  ['status_7',['status',['../structClusterKmeans__Result.html#aade6046154f04e54083c4a4553e8c0c9',1,'ClusterKmeans_Result']]],
-  ['subset_5fproportions_8',['subset_proportions',['../structPerCellQCMetrics__Results.html#af8245c5a21f0aed9ba1a5b87031384f7',1,'PerCellQCMetrics_Results']]],
-  ['subset_5ftotals_9',['subset_totals',['../structPerCellAdtQcMetrics__Results.html#a42200b73d81234e131a7399f722d565d',1,'PerCellAdtQcMetrics_Results']]],
-  ['sums_10',['sums',['../structPerCellAdtQcMetrics__Results.html#a006cf37fc93f12df1421cbbc9404933b',1,'PerCellAdtQcMetrics_Results::sums()'],['../structPerCellQCMetrics__Results.html#aed2914052ca302f343e852dfa7c5f7b3',1,'PerCellQCMetrics_Results::sums()']]]
+  ['scale_5fby_5fneighbors_5findices_0',['scale_by_neighbors_indices',['../scale__by__neighbors_8cpp.html#a484275014c8617f572e626015994e2cd',1,'scale_by_neighbors.cpp']]],
+  ['scale_5fby_5fneighbors_5finternal_1',['scale_by_neighbors_internal',['../scale__by__neighbors_8cpp.html#a31fa5f9dca088f0f39f182184704d803',1,'scale_by_neighbors.cpp']]],
+  ['scale_5fby_5fneighbors_5fmatrices_2',['scale_by_neighbors_matrices',['../scale__by__neighbors_8cpp.html#acdb30ff5064bbbc0d002337c7e57a084',1,'scale_by_neighbors.cpp']]],
+  ['score_5fmarkers_3',['score_markers',['../score__markers_8cpp.html#a96d5665269882aa23e30d033cd659973',1,'score_markers.cpp']]],
+  ['serialize_4',['serialize',['../structNeighborResults.html#af019e13656c3b08923316f07870c6729',1,'NeighborResults']]],
+  ['shape_5',['shape',['../structH5DataSetDetails.html#a01773edafddba4f46022427f8445b64f',1,'H5DataSetDetails::shape()'],['../structLoadedH5DataSet.html#a5e1e0adddab2e5ddac1605c4715941da',1,'LoadedH5DataSet::shape()']]],
+  ['shared_5ffeatures_6',['shared_features',['../classBuiltSinglePPReference.html#aa0e27c099cc5160b31d680d98591f6df',1,'BuiltSinglePPReference']]],
+  ['size_7',['size',['../classJSVector.html#a29628665d2589799afc3389ef06991aa',1,'JSVector::size()'],['../structNeighborResults.html#a210be834f21e0f20f5b373cf5489e5bd',1,'NeighborResults::size()']]],
+  ['sparse_8',['sparse',['../structNumericMatrix.html#a0972e2d156b0e63b625ee6565f6cea8c',1,'NumericMatrix']]],
+  ['status_9',['status',['../structClusterKmeans__Result.html#aade6046154f04e54083c4a4553e8c0c9',1,'ClusterKmeans_Result']]],
+  ['subset_5fproportions_10',['subset_proportions',['../structPerCellQCMetrics__Results.html#af8245c5a21f0aed9ba1a5b87031384f7',1,'PerCellQCMetrics_Results']]],
+  ['subset_5ftotals_11',['subset_totals',['../structPerCellAdtQcMetrics__Results.html#a42200b73d81234e131a7399f722d565d',1,'PerCellAdtQcMetrics_Results']]],
+  ['sums_12',['sums',['../structPerCellAdtQcMetrics__Results.html#a006cf37fc93f12df1421cbbc9404933b',1,'PerCellAdtQcMetrics_Results::sums()'],['../structPerCellQCMetrics__Results.html#aed2914052ca302f343e852dfa7c5f7b3',1,'PerCellQCMetrics_Results::sums()']]]
 ];
