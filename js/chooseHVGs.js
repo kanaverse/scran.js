febc937e6569fe335008f73fb8e50e1cd3df8061
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-02-10 10:28:04 -0800

    Factored out computeTopThreshold for re-use in marker filtering.
    Also added a minimum= argument to chooseHVGs so as to avoid selecting HVGs with
    negative residuals by default... just in case, this usually doesn't happen.

--- js/chooseHVGs.js
+++ js/chooseHVGs.js
@@ -2,6 +2,39 @@ import * as utils from "./utils.js";
 import { ModelGeneVarResults } from "./modelGeneVar.js";
 
 /**
+ * Compute the filter threshold required to retain the top `number` values.
+ *
+ * @param {Array|TypedArray} x - Array of numeric values, typically per-gene statistics.
+ * @param {number} number - Number of top values to retain after filtering on the returned threshold.
+ * This may be approximate in the presence of ties.
+ * @param {object} [options={}] - Optional parameters.
+ * @param {boolean} [options.largest=true] - Whether the top values in `x` are defined as the largest numeric values.
+ * If `false`, the top values are considered to be the smallest.
+ *
+ * @return {number} Threshold to be applied to `x` to obtain the top (approximately) `number` values.
+ * If `largest = true`, filtering is performed by taking all values in `x` that are greater than or equal to the returned threshold;
+ * if `false`, filtering is performed by taking all values in `x` that are less than or equal to the returned threshold.
+ * If `x` is zero-length, NaN is returned.
+ */
+export function computeTopThreshold(x, number, { largest = true } = {}) {
+    if (x instanceof Array) {
+        x.sort((a, b) => a - b); // just in case...
+    } else {
+        x.sort();
+    }
+
+    if (x.length == 0) {
+        return Number.NaN;
+    }
+
+    if (largest) {
+        return x[Math.max(0, x.length - number)]; 
+    } else {
+        return x[Math.min(number, x.length) - 1]; 
+    }
+}
+
+/**
  * Choose the highly variable genes from variance modelling statistics.
  *
  * @param {(TypedArray|ModelGeneVarResults)} x -
@@ -9,11 +42,13 @@ import { ModelGeneVarResults } from "./modelGeneVar.js";
  * or a {@linkplain ModelGeneVarResults} object, in which case the residuals are used as the statistics.
  * @param {object} [options={}] - Optional parameters.
  * @param {number} [options.number=4000] - Number of highly variable genes to select.
+ * @param {number} [options.minimum=0] - Minimum value of the residual to consider for a highly variable gene.
+ * By default, a highly variable gene must have a non-negative residual.
  *
  * @return {Uint8WasmArray} Array of length equal to the total number of genes,
  * where the chosen highly variable genes are marked with a value of 1 and all other genes have values of zero.
  */
-export function chooseHVGs(x, { number = 4000 } = {}) {
+export function chooseHVGs(x, { number = 4000, minimum = 0 } = {}) {
     let stat; 
     if (x instanceof ModelGeneVarResults) {
         stat = x.residuals();
@@ -21,8 +56,10 @@ export function chooseHVGs(x, { number = 4000 } = {}) {
         stat = x.slice();
     }
 
-    stat.sort();
-    let threshold = stat[Math.max(0, stat.length - number)]; 
+    let threshold = computeTopThreshold(stat, number, { largest: true });
+    if (threshold < minimum) {
+        threshold = minimum;
+    }
 
     let features = utils.createUint8WasmArray(stat.length);
     try {
--- tests/chooseHVGs.test.js
+++ tests/chooseHVGs.test.js
@@ -37,3 +37,39 @@ test("chooseHVGs works correctly", () => {
     res.free();
     output.free();
 });
+
+test("chooseHVGs avoids picking negative residuals", () => {
+    let res = [ 0, 2, 3, 4, -1 ];
+    var output = scran.chooseHVGs(res, { number: 101 });
+
+    let expected = new Uint8Array(res.length);
+    expected.fill(1);
+    expected[4] = 0;
+    expect(output.array()).toEqual(expected);
+
+    let output2 = scran.chooseHVGs(res, { number: 101, minimum: -1 });
+    expected[4] = 1;
+    expect(output2.array()).toEqual(expected);
+
+    // Cleaning up.
+    output.free();
+    output2.free();
+})
+
+test("computeTopThreshold works correctly", () => {
+    expect(scran.computeTopThreshold([1,2,3,4,5], 1)).toEqual(5);
+    expect(scran.computeTopThreshold([5,2,3,4,1], 2)).toEqual(4);
+    expect(scran.computeTopThreshold([5,2,3,4,1], 100)).toEqual(1);
+    expect(scran.computeTopThreshold([5,1,4,3,2], 1, { largest: false })).toEqual(1);
+    expect(scran.computeTopThreshold([3,4,5,1,2], 2, { largest: false })).toEqual(2);
+    expect(scran.computeTopThreshold([3,4,5,1,2], 200, { largest: false })).toEqual(5);
+
+    // Same for typed arrays.
+    expect(scran.computeTopThreshold(new Float64Array([5,2,1,4,3]), 1)).toEqual(5);
+    expect(scran.computeTopThreshold(new Float64Array([3,2,5,4,1]), 2)).toEqual(4);
+    expect(scran.computeTopThreshold(new Float64Array([2,1,3,4,5]), 1, { largest: false })).toEqual(1);
+    expect(scran.computeTopThreshold(new Float64Array([4,3,2,5,1]), 2, { largest: false })).toEqual(2);
+
+    expect(scran.computeTopThreshold([], 20)).toEqual(Number.NaN);
+})
+
