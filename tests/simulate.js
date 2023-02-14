fd4ba0a45d575b65964f1604ef49e23f9816580d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-02-13 22:43:36 -0800

    Avoid int overflow when injecting big values for layered loading tests.

--- tests/simulate.js
+++ tests/simulate.js
@@ -1,6 +1,6 @@
 import * as scran from "../js/index.js";
 
-export function simulateSparseData(primary, secondary, injectBigValues = false) {
+export function simulateSparseData(primary, secondary, injectBigValues = false, forceInteger = true) {
     let data = [];
     let indices = [];
     let indptrs = new Uint32Array(primary + 1);
@@ -30,17 +30,11 @@ export function simulateSparseData(primary, secondary, injectBigValues = false) 
         }
     }
 
-    let data2 = new Uint16Array(data.length);
-    data2.set(data);
-    let indices2 = new Int32Array(indices.length);
-    indices2.set(indices);
-    let indptrs2 = new Uint32Array(indptrs.length);
-    indptrs2.set(indptrs);
-
+    let data_constructor = (forceInteger ? Uint32Array : Float64Array);
     return {
-        "data": data2,
-        "indices": indices2,
-        "indptrs": indptrs2
+        "data": new data_constructor(data),
+        "indices": new Int32Array(indices),
+        "indptrs": indptrs
     };
 }
 
