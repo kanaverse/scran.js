f9b94691e6f217dd326090ff9ec6419e2e042aee
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-02-23 11:25:11 -0800

    Expanded the range of guessable features, mostly based on Ensembl.

--- js/guessFeatures.js
+++ js/guessFeatures.js
@@ -3,59 +3,178 @@
  *
  * @param {Array} features - Array of strings containing feature identifiers, typically Ensembl IDs or gene symbols.
  * Elements may also be `null` or undefined if an identifier is missing.
+ * @param {object} [options={}] - Optional parameters.
+ * @param {boolean} [options.forceTaxonomy=false] - Whether to force the use of taxonomy IDs for human and mouse.
+ * This is `false` for back compatibility.
  *
- * @return {object} An object containing the inferred `species`, which can be either `"human"` or `"mouse"`;
- * and the identifier `type`, which can be either `"ensembl"` or `"symbol"`.
- * A `confidence` value is reported which defines the percentage of entries in `x` that are consistent with the inferred identity.
+ * @return {object} An object containing:
+ *
+ * - `species`, the inferred species as a string.
+ *   This can be either `"human"` or `"mouse"`, or an NCBI taxonomy ID (one of 6239, 10116, 9541, 7227, 7955, 9598).
+ *   If `forceTaxonomy = true`, human and mouse are replaced with 9606 and 10090, respectively.
+ * - `type`: the feature identifier type.
+ *   This can either be `"ensembl"` or `"symbol"`.
+ * - `confidence`: the percentage of entries in `x` that are consistent with the inferred identity.
  */
-export function guessFeatures(features) {
-    // Human Ensembl.
-    let human_ens = 0
+export function guessFeatures(features, { forceTaxonomy = false } = {}) {
+    let ntotal = features.length;
+    let early_threshold = Math.ceil(ntotal / 2);
+    let format = payload => {
+        payload.confidence /= ntotal;
+        return payload;
+    };
+
+    let ensembl_human = 0;
+    let ensembl_mouse = 0;
+    let ensembl_6239 = 0;
+    let ensembl_10116 = 0; // Ensembl only, Rat symbols are indistiguishable from mice.
+    let ensembl_9541 = 0; // Ensembl only, Mfac symbols are indistiguishable from human.
+    let ensembl_7227 = 0; // Ensembl only, fly symbols are crazy.
+    let ensembl_7955 = 0;
+    let ensembl_9598 = 0; // Ensembl only, Chimp symbols are indistinguishable from human.
+
+    let symbol_human = 0;
+    let symbol_mouse = 0;
+    let symbol_6239 = 0;
+    let symbol_7955 = 0;
+
+    let hsid = (forceTaxonomy ? "9606" : "human");
+    let mmid = (forceTaxonomy ? "10090" : "mouse");
+    let collected = [];
+
+    // Checking if it's any type of Ensembl.
+    let any_ens = 0;
     features.forEach(x => {
-        if (x && x.match(/^ENSG[0-9]{11}$/)) {
-            human_ens++;
+        if (x && x.match(/^ENS[A-Z]*G[0-9]{11}$/)) {
+            any_ens++;
         }
     });
 
-    // Human symbol; starts with upper case, no lower case, and not an Ensembl of any kind.
-    let human_sym = 0
-    features.forEach(x => {
-        if (x && x.match(/^[A-Z][^a-z]+$/) && !x.match(/^ENS[A-Z]+[0-9]{11}/)) {
-            human_sym++;
-        }
-    });
+    if (any_ens) {
+        features.forEach(x => {
+            if (x) {
+                if (x.startsWith("ENSG")) {
+                    ensembl_human++;
+                } else if (x.startsWith("ENSMUSG")) {
+                    ensembl_mouse++;
+                } else if (x.startsWith("ENSRNOG")) {
+                    ensembl_10116++;
+                } else if (x.startsWith("ENSMFAG")) {
+                    ensembl_9541++;
+                } else if (x.startsWith("ENSDARG")) {
+                    ensembl_7955++;
+                } else if (x.startsWith("ENSPTRG")) {
+                    ensembl_9598++;
+                }
+            }
+        });
 
-    // Mouse Ensembl.
-    let mouse_ens = 0
-    features.forEach(x => {
-        if (x && x.match(/^ENSMUSG[0-9]{11}$/)) {
-            mouse_ens++;
-        }
-    });
+        collected.push({ species: hsid, type: "ensembl", confidence: ensembl_human });
+        collected.push({ species: mmid, type: "ensembl", confidence: ensembl_mouse });
+        collected.push({ species: "10116", type: "ensembl", confidence: ensembl_10116 });
+        collected.push({ species: "9541", type: "ensembl", confidence: ensembl_9541 });
+        collected.push({ species: "7955", type: "ensembl", confidence: ensembl_7955 });
+        collected.push({ species: "9598", type: "ensembl", confidence: ensembl_9598 });
 
-    // Mouse symbol; starts with upper case, but no upper case after that.
-    let mouse_sym = 0
-    features.forEach(x => {
-        if (x && x.match(/^[A-Z][^A-Z]+$/)) {
-            mouse_sym++;
-        }
-    });
-
-    // Who's the highest?
-    let output = [
-        { "species": "human", "type": "ensembl", "confidence": human_ens},
-        { "species": "human", "type": "symbol", "confidence": human_sym},
-        { "species": "mouse", "type": "ensembl", "confidence": mouse_ens},
-        { "species": "mouse", "type": "symbol", "confidence": mouse_sym}
-    ];
-
-    let highest = output[0];
-    for (var i = 1; i < output.length; i++) {
-        if (output[i].confidence > highest.confidence) {
-            highest = output[i];
+        // See if we can quit early and avoid the other checks.
+        for (const x of collected) {
+            if (x.confidence >= early_threshold) {
+                return format(x);
+            }
         }
     }
 
-    highest.confidence /= features.length;
-    return highest;
+    // Human symbol; starts with upper case, no lower case, and not an Ensembl of any kind.
+    features.forEach(x => {
+        if (x && x.match(/^[A-Z][^a-z]+$/) && !x.match(/^ENS[A-Z]+[0-9]{11}/)) {
+            symbol_human++;
+        }
+    });
+    {
+        let payload = { species: hsid, type: "symbol", confidence: symbol_human };
+        if (payload.confidence >= early_threshold) {
+            return format(payload);
+        }
+        collected.push(payload);
+    }
+
+    // Mouse symbol; starts with upper case, but no upper case after that.
+    features.forEach(x => {
+        if (x && x.match(/^[A-Z][^A-Z]+$/)) {
+            symbol_mouse++;
+        }
+    });
+    {
+        let payload = { species: mmid, type: "symbol", confidence: symbol_mouse };
+        if (payload.confidence >= early_threshold) {
+            return format(payload);
+        }
+        collected.push(payload);
+    }
+
+    // Worm Ensembl (WormBase).
+    features.forEach(x => {
+        if (x && x.match(/^WBGene[0-9]+$/)) {
+            ensembl_6239++;
+        }
+    });
+    {
+        let payload = { species: "6239", type: "ensembl", confidence: ensembl_6239 };
+        if (payload.confidence >= early_threshold) {
+            return format(payload);
+        }
+        collected.push(payload);
+    }
+
+    // Fly Ensembl (FlyBase).
+    features.forEach(x => {
+        if (x && x.match(/^FBgn[0-9]+$/)) {
+            ensembl_7227++;
+        }
+    });
+    {
+        let payload = { species: "7227", type: "ensembl", confidence: ensembl_7227 };
+        if (payload.confidence >= early_threshold) {
+            return format(payload);
+        }
+        collected.push(payload);
+    }
+
+    // Worm symbols; at least three lower case with a dash and numbers.
+    features.forEach(x => {
+        if (x && x.match(/^[a-z]{3,}-[0-9]+$/)) {
+            symbol_6239++;
+        }
+    });
+    {
+        let payload = { species: "6239", type: "symbol", confidence: symbol_6239 };
+        if (payload.confidence >= early_threshold) {
+            return format(payload);
+        }
+        collected.push(payload);
+    }
+
+    // Zebrafish symbols; at least three lower case letters, no dash, followed by numbers and/or more lower case.
+    features.forEach(x => {
+        if (x && x.match(/^[a-z]{3,}[0-9a-z]+$/)) {
+            symbol_7955++;
+        }
+    });
+    {
+        let payload = { species: "7955", type: "symbol", confidence: symbol_7955 };
+        if (payload.confidence >= early_threshold) {
+            return format(payload);
+        }
+        collected.push(payload);
+    }
+
+    // Picking the best.
+    let highest = collected[0];
+    for (var i = 1; i < collected.length; i++) {
+        if (collected[i].confidence > highest.confidence) {
+            highest = collected[i];
+        }
+    }
+
+    return format(highest);
 }
--- tests/features.test.js
+++ tests/features.test.js
@@ -4,7 +4,7 @@ import * as compare from "./compare.js";
 beforeAll(async () => { await scran.initialize({ localFile: true }) });
 afterAll(async () => { await scran.terminate() });
 
-test("guessFeatures works as expected", () => {
+test("guessFeatures works as expected for human and mouse", () => {
     let output = scran.guessFeatures(["ENSG00000000003", "ENSG00000230003", "asdasd"]);
     expect(output.confidence).toBe(2/3);
     expect(output.type).toBe("ensembl");
@@ -24,7 +24,57 @@ test("guessFeatures works as expected", () => {
     expect(output.confidence).toBe(3/4);
     expect(output.type).toBe("symbol");
     expect(output.species).toBe("mouse");
+
+    // Force the taxonomy.
+    output = scran.guessFeatures(["ENSG00000000003", "ENSG00000230003", "ENSG00111010210", "ENSG00000000004"], { forceTaxonomy: true });
+    expect(output.species).toBe("9606");
+    output = scran.guessFeatures(["Snap25", "Malat1", "ENSMUSG00111010210", "Cd8a"], { forceTaxonomy: true });
+    expect(output.species).toBe("10090");
 });
+
+test("guessFeatures works as expected for some more exotic Ensembls", () => {
+    let output = scran.guessFeatures(["ENSMFAG00000000003", "ENSMFAG00000230003", "asdasd"]);
+    expect(output.confidence).toBe(2/3);
+    expect(output.type).toBe("ensembl");
+    expect(output.species).toBe("9541");
+
+    output = scran.guessFeatures(["ENSRNOG00000000003", "ENSRNOG00000230003", "asdasd"]);
+    expect(output.confidence).toBe(2/3);
+    expect(output.type).toBe("ensembl");
+    expect(output.species).toBe("10116");
+
+    output = scran.guessFeatures(["Rps-2", "ENSDARG00000000003", "ENSDARG00000230003", "asdasd"]);
+    expect(output.confidence).toBe(2/4);
+    expect(output.type).toBe("ensembl");
+    expect(output.species).toBe("7955");
+
+    output = scran.guessFeatures(["BLAH", "ENSPTRG00000000003", "ENSPTRG00000230003", "asdasd"]);
+    expect(output.confidence).toBe(2/4);
+    expect(output.type).toBe("ensembl");
+    expect(output.species).toBe("9598");
+
+    output = scran.guessFeatures(["FBgn0035146", "FBgn0031106", "asdasd"]);
+    expect(output.confidence).toBe(2/3);
+    expect(output.type).toBe("ensembl");
+    expect(output.species).toBe("7227");
+
+    output = scran.guessFeatures(["WBGene00001115", "WBGene00021460", "asdasd"]);
+    expect(output.confidence).toBe(2/3);
+    expect(output.type).toBe("ensembl");
+    expect(output.species).toBe("6239");
+})
+
+test("guessFeatures works as expected for some other symbols", () => {
+    let output = scran.guessFeatures(["snap-26", "rbc-55", "act-2"]);
+    expect(output.confidence).toBe(1);
+    expect(output.type).toBe("symbol");
+    expect(output.species).toBe("6239");
+
+    output = scran.guessFeatures(["snap26", "rbc55a", "agrp"]);
+    expect(output.confidence).toBe(1);
+    expect(output.type).toBe("symbol");
+    expect(output.species).toBe("7955");
+})
 
 test("guessFeatures handles null values properly", () => {
     let output = scran.guessFeatures(["ENSG00000000003", "ENSG00000230003", null]);
