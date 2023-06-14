b4fec7650512db2e765f03f24fe7cfa9f32a26b4
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2023-06-14 15:51:43 -0700

    Added options to subset by rows/columns when loading a HDF5 matrix. (#77)
    This allows us to load a submatrix directly, without the need for delayed layers.
    This can save memory and improve access performance, e.g., if the subset would
    otherwise introduce a non-consecutive access pattern in the underlying matrix.

--- src/utils.h
+++ src/utils.h
@@ -17,4 +17,15 @@ std::vector<T> convert_array_of_offsets(size_t n, uintptr_t x) {
     return output;
 }
 
+template<bool row>
+void check_subset_indices(const int* ptr, size_t len, size_t limit) {
+    for (size_t i = 0; i < len; ++i) {
+        if (ptr[i] < 0) {
+            throw std::runtime_error("subset indices should be non-negative");
+        } else if (ptr[i] >= limit) {
+            throw std::runtime_error("subset indices should be less than the number of " + (row ? std::string("rows") : std::string("columns")));
+        }
+    }
+}
+
 #endif
