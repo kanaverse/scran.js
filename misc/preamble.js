f41ef0013a7162f961f1eeb4df21c0bbb709c13d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-10-02 11:04:53 -0700

    Continue the grind to reduce the build times.

--- misc/preamble.js
+++
@@ -1,6 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
-import { URL } from 'url';
-const __filename = new URL('', import.meta.url).pathname;
-const __dirname = new URL('', import.meta.url).pathname;
