ecd2b28b4471ee34b55e09e7fc3843e0bff0d946
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2024-10-02 11:21:00 -0700

    Switch back to internal builds of igraph and HDF5. (#89)
    This avoids headaches from guessing ABI compatibility across different
    Emscripten versions and build flags. With suitable caching, we can avoid
    achieve prebuilt binaries but with more suitable integration with our own CI.
    
    We also abandon the dedicated scran.js Docker image in favor of the generic
    emsdk + CMake image. This reduces the complexity of the CI and avoids stale
    object files in the CMake build. We use more caching to save the Wasm binary so
    that it doesn't need to be rebuilt if the C++ code hasn't changed.
    
    Also got rid of preamble.js as it's no longer needed in the new toolchain.

---
+++ extern/igraph/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -u
+
+IGRAPH_VERSION=0.10.13
+IGRAPH_HASH=c6dc44324f61f52c098bedb81f6a602365d39d692d5068ca4fc3734b2a15e64c
+SOURCE_DIR=igraph-${IGRAPH_VERSION}
+
+if [[ ! -e ${SOURCE_DIR} ]]
+then
+    wget -q https://github.com/igraph/igraph/releases/download/${IGRAPH_VERSION}/igraph-${IGRAPH_VERSION}.tar.gz -O igraph.tar.gz
+    OBSERVED_HASH=($(shasum -a 256 igraph.tar.gz))
+    if [[ ${OBSERVED_HASH} != ${IGRAPH_HASH} ]]
+    then
+        echo "hash mismatch for ${IGRAPH_VERSION} (got ${OBSERVED_HASH})"
+        exit 1
+    fi
+    tar -xf igraph.tar.gz
+fi
+
+BUILD_DIR=build-${IGRAPH_VERSION}
+if [ ! -e ${BUILD_DIR} ]
+then
+    mkdir -p ../installed
+    coreflags="-pthread" # propagating compile flags from the root scran.js CMakeLists.txt.
+    echo "{}" > package.json # avoid assuming ES6 module syntax from the root scran.js package.json.
+    emcmake cmake \
+        -S ${SOURCE_DIR} \
+        -B ${BUILD_DIR} \
+        -DCMAKE_C_FLAGS="${coreflags}" \
+        -DCMAKE_CXX_FLAGS="${coreflags}" \
+        -DIGRAPH_WARNINGS_AS_ERRORS=OFF \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=$(pwd)/../installed
+fi
+
+cd ${BUILD_DIR}
+emmake make
+emmake make install
