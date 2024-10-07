82b9272b0e84f337b7311770b48f8fa364d87abc
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-10-07 14:54:01 -0700

    Bring the hypergeometric tests back online.

--- js/hypergeometricTest.js
+++ js/hypergeometricTest.js
@@ -11,15 +11,14 @@ import * as wasm from "./wasm.js";
  * @param {number|Array|TypedArray|WasmArray} featureSetSize - Size of the feature set.
  * @param {number|Array|TypedArray|WasmArray} numberOfFeatures - Total number of features.
  * @param {object} [options={}] - Optional parameters.
+ * @param {boolean} [options.log=false] - Whether to compute log-probabilities.
  * @param {?number} [options.numberOfThreads=null] - Number of threads to use.
  * If `null`, defaults to {@linkcode maximumThreads}.
- * @param {boolean} [options.assumeSorted=false] - Whether the input arrays are already sorted such that `markersInSet` is the slowest-changing value.
- * Setting this to `true` can avoid an extra sorting step for greater efficiency.
  *
- * @return {Float64Array} An array of length equal to that of the supplied arrays (or 1, if no arrays are supplied).
+ * @return {Float64WasmArray} An array of length equal to that of the supplied arrays (or 1, if no arrays are supplied).
  * The i-th entry contains the p-value for enrichment computed using the i-th entry of each supplied array. 
  */
-export function hypergeometricTest(markersInSet, numberOfMarkers, featureSetSize, numberOfFeatures, { numberOfThreads = null, assumeSorted = false } = {}) {
+export function hypergeometricTest(markersInSet, numberOfMarkers, featureSetSize, numberOfFeatures, { log = false, numberOfThreads = null } = {}) {
     let markersInSet_data;
     let numberOfMarkers_data;
     let featureSetSize_data;
@@ -63,14 +62,12 @@ export function hypergeometricTest(markersInSet, numberOfMarkers, featureSetSize
     }
 
     let output;
-    let output_data;
-
     try {
         markersInSet_data = utils.wasmifyArray(markersInSet, "Int32WasmArray");
         numberOfMarkers_data = utils.wasmifyArray(numberOfMarkers, "Int32WasmArray");
         featureSetSize_data = utils.wasmifyArray(featureSetSize, "Int32WasmArray");
         numberOfFeatures_data = utils.wasmifyArray(numberOfFeatures, "Int32WasmArray");
-        output_data = utils.createFloat64WasmArray(ntests);
+        output = utils.createFloat64WasmArray(ntests);
 
         wasm.call(module => module.hypergeometric_test(
             ntests, 
@@ -82,19 +79,16 @@ export function hypergeometricTest(markersInSet, numberOfMarkers, featureSetSize
             numberOfMarkers_data.offset,
             numberOfFeatures_data.length != 1,
             numberOfFeatures_data.offset,
-            assumeSorted,
-            output_data.offset,
+            output.offset,
+            log,
             nthreads
         ));
-
-        output = output_data.slice();
 
     } finally {
         utils.free(markersInSet_data);
         utils.free(numberOfMarkers_data);
         utils.free(featureSetSize_data);
         utils.free(numberOfFeatures_data);
-        utils.free(output_data);
     }
 
     return output;
--- src/hypergeometric_test.cpp
+++ src/hypergeometric_test.cpp
@@ -2,9 +2,8 @@
 
 #include "NumericMatrix.h"
 #include "utils.h"
-#include "parallel.h"
-
-#include "scran/scran.hpp"
+#include "subpar/subpar.hpp"
+#include "phyper/phyper.hpp"
 
 void hypergeometric_test(
     int ntests,
@@ -16,79 +15,36 @@ void hypergeometric_test(
     uintptr_t num_markers, 
     bool multi_num_features, 
     uintptr_t num_features, 
-    bool sorted,
     uintptr_t output,
+    bool log, 
     int nthreads) 
 {
     const int32_t* misptr = reinterpret_cast<const int32_t*>(markers_in_set);
     const int32_t* ssptr = reinterpret_cast<const int32_t*>(set_size);
     const int32_t* nmptr = reinterpret_cast<const int32_t*>(num_markers);
     const int32_t* nfptr = reinterpret_cast<const int32_t*>(num_features);
-
-    std::vector<int> indices(ntests);
-    std::iota(indices.begin(), indices.end(), 0);
-
-    if (!sorted) {
-        std::sort(indices.begin(), indices.end(), [&](int l, int r) -> bool {
-            if (multi_num_features) {
-                if (nfptr[l] < nfptr[r]) {
-                    return true;
-                } else if (nfptr[l] > nfptr[r]) {
-                    return false;
-                }
-            }
-
-            if (multi_num_markers) {
-                if (nmptr[l] < nmptr[r]) {
-                    return true;
-                } else if (nmptr[l] > nmptr[r]) {
-                    return false;
-                }
-            }
-
-            if (multi_set_size) {
-                if (ssptr[l] < ssptr[r]) {
-                    return true;
-                } else if (ssptr[l] > ssptr[r]) {
-                    return false;
-                }
-            }
-
-            if (multi_markers_in_set) {
-                if (misptr[l] < misptr[r]) {
-                    return true;
-                } else if (misptr[l] > misptr[r]) {
-                    return false;
-                }
-            }
-
-            return l < r;
-        });
-    }
-
     double* outptr = reinterpret_cast<double*>(output);
-    scran::HypergeometricTail hyper;
 
-    run_parallel_old(ntests, [&](int first, int last) -> void {
-        for (int i = first; i < last; i++) {
-            auto x = indices[i];
+    subpar::parallelize_range(nthreads, ntests, [&](int, int first, int length) {
+        phyper::Options hopt;
+        hopt.log = log;
 
-            // We'll interpret the features in the set as white balls,
+        for (int i = first, last = first + length; i < last; ++i) {
+            // We'll interpret the genes in the set as white balls,
             // the features _not_ in the set as black balls,
             // and the number of markers as the number of draws.
-            auto num_white = ssptr[multi_set_size ? x : 0];
-            auto num_black = nfptr[multi_num_features ? x : 0] - num_white;
+            auto num_white = ssptr[multi_set_size ? i : 0];
+            auto num_black = nfptr[multi_num_features ? i : 0] - num_white;
 
-            outptr[x] = hyper.run(
-                misptr[multi_markers_in_set ? x : 0],
+            outptr[i] = phyper::compute(
+                misptr[multi_markers_in_set ? i : 0],
                 num_white,
                 num_black,
-                nmptr[multi_num_markers ? x : 0]
+                nmptr[multi_num_markers ? i : 0],
+                hopt
             );
         }
-    }, nthreads);
-
-    return;
+    }); 
 }
 
 EMSCRIPTEN_BINDINGS(hypergeometric_test) {
