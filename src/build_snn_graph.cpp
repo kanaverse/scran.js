0cc69a38bc906bd6690ea6f1b9f8ee94245c87cc
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-10-10 10:46:45 -0700

    Forgot to add the build_snn_graph files to the repo.

---
+++ src/build_snn_graph.cpp
@@ -0,0 +1,30 @@
+#include <emscripten/bind.h>
+
+#include <stdexcept>
+#include <string>
+
+#include "NeighborIndex.h"
+#include "build_snn_graph.h"
+
+BuildSnnGraphResult build_snn_graph(const NeighborResults& neighbors, std::string scheme, int nthreads) {
+    scran_graph_cluster::BuildSnnGraphOptions opt;
+    opt.num_threads = nthreads;
+
+    if (scheme == "rank") {
+        opt.weighting_scheme = scran_graph_cluster::SnnWeightScheme::RANKED;
+    } else if (scheme == "number") {
+        opt.weighting_scheme = scran_graph_cluster::SnnWeightScheme::NUMBER;
+    } else if (scheme == "jaccard") {
+        opt.weighting_scheme = scran_graph_cluster::SnnWeightScheme::JACCARD;
+    } else {
+        throw std::runtime_error("no known weighting scheme '" + scheme + "'");
+    }
+
+    return BuildSnnGraphResult(scran_graph_cluster::build_snn_graph(neighbors.neighbors, opt));
+}
+
+EMSCRIPTEN_BINDINGS(build_snn_graph) {
+    emscripten::function("build_snn_graph", &build_snn_graph, emscripten::return_value_policy::take_ownership());
+
+    emscripten::class_<BuildSnnGraphResult>("BuildSnnGraphResult");
+}
